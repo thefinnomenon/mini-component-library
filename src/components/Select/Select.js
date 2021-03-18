bfcd02dc207cdc3da711c9afa37ea35003b869a1
Chris Finn <chris@finn.com>
2021-03-18 14:40:50 -0300

    Implement Select component.

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,61 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <Presentation>
+        {displayedValue}
+        <IconWrapper>
+          <Icon id='chevron-down' stroke={1} size={24} />
+        </IconWrapper>
+      </Presentation>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0%;
+`;
+
+const Presentation = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+  font-size: ${16 / 16}rem;
+  padding: 12px 16px;
+  padding-right: 52px;
+  border-radius: 8px;
+
+  ${NativeSelect}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black}
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 10px;
+  margin: auto;
+  width: 24px;
+  height: 24px;
+  pointer-events: none;
+`;
 
 export default Select;
